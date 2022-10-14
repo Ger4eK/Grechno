eb68e4e554bc819262aa359a5274eebd1c65774e
Ігор Мельник <ihor@nyhetstjenester.no>
2022-10-14 22:31:32 +0300

    image & product fix

--- src/components/MainPage/Catalog.tsx
+++ src/components/MainPage/Catalog.tsx
@@ -1,164 +1,186 @@
-import arrow from '../../assets/images/arrow.svg';
+import arrow from "../../assets/images/arrow.svg";
 
 const Catalog = () => {
   return (
-    <div className='catalog'>
-      <div className='catalog__container'>
-        <div className='catalog__wrapper'>
-          <div className='catalog__categories'>
-            <h1 className='categories-title'>КАТАЛОГ</h1>
-            <ul className='categories-nav'>
+    <div className="catalog">
+      <div className="catalog__container">
+        <div className="catalog__wrapper">
+          <div className="catalog__categories">
+            <h1 className="categories-title">КАТАЛОГ</h1>
+            <ul className="categories-nav">
               <li>
-                <a className='categories-link active' href='#'>
+                <a className="categories-link active" href="#">
                   Все
                 </a>
               </li>
               <li>
-                <a className='categories-link' href='#'>
+                <a className="categories-link" href="#">
                   Шопери
                 </a>
               </li>
               <li>
-                <a className='categories-link' href='#'>
+                <a className="categories-link" href="#">
                   Авоськи
                 </a>
               </li>
               <li>
-                <a className='categories-link' href='#'>
+                <a className="categories-link" href="#">
                   Набори
                 </a>
               </li>
               <li>
-                <a className='categories-link' href='#'>
+                <a className="categories-link" href="#">
                   Розпродаж
                 </a>
               </li>
             </ul>
           </div>
-          <ul className='products-list'>
-            <li className='products-item'>
-              <a className='products-item-img' href='#'>
+          <ul className="products-list">
+            <li className="products-item">
+              <a className="products-item-img" href="#">
                 <img
-                  src={require('../../assets/images/products/prod-4.jpg')}
-                  alt='product'
+                  src={require("../../assets/images/products/prod-4.jpg")}
+                  alt="product"
                 />
               </a>
-              <div className='products-item-info'>
-                <div className='products-item-left'>
-                  <h1 className='products-item-title'>Набір “ОСІНЬ”</h1>
-                  <p className='products-item-prod'>Авоська</p>
-                  <p className='products-item-prod'>Шопер</p>
+              <div className="products-item-info">
+                <div className="products-item-wrapper">
+                  <div className="products-item-left">
+                    <h1 className="products-item-title">Набір “ОСІНЬ”</h1>
+                    <div>
+                      <p className="products-item-prod">Авоська</p>
+                      <p className="products-item-prod">Шопер</p>
+                    </div>
+                  </div>
+                  <div className="products-item-right">
+                    <a className="products-item-arrow" href="#">
+                      <img src={arrow} alt="arrow" />
+                    </a>
+                  </div>
                 </div>
-                <div className='products-item-right'>
-                  <a className='products-item-arrow' href='#'>
-                    <img src={arrow} alt='arrow' />
-                  </a>
-                  <span className='products-item-price'>380 грн</span>
-                </div>
+                <span className="products-item-price">380 грн</span>
               </div>
             </li>
-            <li className='products-item'>
-              <a className='products-item-img' href='#'>
+            <li className="products-item">
+              <a className="products-item-img" href="#">
                 <img
-                  src={require('../../assets/images/products/prod-1.jpg')}
-                  alt='product'
+                  src={require("../../assets/images/products/prod-1.jpg")}
+                  alt="product"
                 />
               </a>
-              <div className='products-item-info'>
-                <div className='products-item-left'>
-                  <h1 className='products-item-title'>Набір “ВЕСНА”</h1>
-                  <p className='products-item-prod'>Авоська</p>
-                  <p className='products-item-prod'>Еко чашка</p>
-                  <p className='products-item-prod'>Мішечок</p>
+              <div className="products-item-info">
+                <div className="products-item-wrapper">
+                  <div className="products-item-left">
+                    <h1 className="products-item-title">Набір “ВЕСНА”</h1>
+                    <div>
+                      <p className="products-item-prod">Авоська</p>
+                      <p className="products-item-prod">Еко чашка</p>
+                      <p className="products-item-prod">Еко чашка</p>
+                      <p className="products-item-prod">Еко чашка</p>
+                      <p className="products-item-prod">Мішечок</p>
+                    </div>
+                  </div>
+                  <div className="products-item-right">
+                    <a className="products-item-arrow" href="#">
+                      <img src={arrow} alt="arrow" />
+                    </a>
+                  </div>
                 </div>
-                <div className='products-item-right'>
-                  <a className='products-item-arrow' href='#'>
-                    <img src={arrow} alt='arrow' />
-                  </a>
-                  <span className='products-item-price'>350 грн</span>
-                </div>
+                <span className="products-item-price">350 грн</span>
               </div>
             </li>
-            <li className='products-item'>
-              <a className='products-item-img' href='#'>
+            <li className="products-item">
+              <a className="products-item-img" href="#">
                 <img
-                  src={require('../../assets/images/products/prod-2.jpg')}
-                  alt='product'
+                  src={require("../../assets/images/products/prod-2.jpg")}
+                  alt="product"
                 />
               </a>
-              <div className='products-item-info'>
-                <div className='products-item-left'>
-                  <h1 className='products-item-title'>Набір “ОСНОВА”</h1>
-                  <p className='products-item-prod'>Авоська</p>
-                  <p className='products-item-prod'>Шопер</p>
+              <div className="products-item-info">
+                <div className="products-item-wrapper">
+                  <div className="products-item-left">
+                    <h1 className="products-item-title">Набір “ОСНОВА”</h1>
+                    <div>
+                      <p className="products-item-prod">Авоська</p>
+                      <p className="products-item-prod">Шопер</p>
+                    </div>
+                  </div>
+                  <div className="products-item-right">
+                    <a className="products-item-arrow" href="#">
+                      <img src={arrow} alt="arrow" />
+                    </a>
+                  </div>
                 </div>
-                <div className='products-item-right'>
-                  <a className='products-item-arrow' href='#'>
-                    <img src={arrow} alt='arrow' />
-                  </a>
-                  <span className='products-item-price'>450 грн</span>
-                </div>
+                <span className="products-item-price">450 грн</span>
               </div>
             </li>
-            <li className='products-item'>
-              <a className='products-item-img' href='#'>
+            <li className="products-item">
+              <a className="products-item-img" href="#">
                 <img
-                  src={require('../../assets/images/products/prod-3.jpg')}
-                  alt='product'
+                  src={require("../../assets/images/products/prod-3.jpg")}
+                  alt="product"
                 />
               </a>
-              <div className='products-item-info'>
-                <div className='products-item-left'>
-                  <h1 className='products-item-title'>Шопер з розписом</h1>
-                  <p className='products-item-prod'>Розмір 200х300х10 мм</p>
+              <div className="products-item-info">
+                <div className="products-item-wrapper">
+                  <div className="products-item-left">
+                    <h1 className="products-item-title">Шопер з розписом</h1>
+                    <div>
+                      <p className="products-item-prod">Розмір 200х300х10 мм</p>
+                    </div>
+                  </div>
+                  <div className="products-item-right">
+                    <a className="products-item-arrow" href="#">
+                      <img src={arrow} alt="arrow" />
+                    </a>
+                  </div>
                 </div>
-                <div className='products-item-right'>
-                  <a className='products-item-arrow' href='#'>
-                    <img src={arrow} alt='arrow' />
-                  </a>
-                  <span className='products-item-price'>380 грн</span>
-                </div>
+                <span className="products-item-price">380 грн</span>
               </div>
             </li>
-            <li className='products-item'>
-              <a className='products-item-img' href='#'>
+            <li className="products-item">
+              <a className="products-item-img" href="#">
                 <img
-                  src={require('../../assets/images/products/prod-5.jpg')}
-                  alt='product'
+                  src={require("../../assets/images/products/prod-5.jpg")}
+                  alt="product"
                 />
               </a>
-              <div className='products-item-info'>
-                <div className='products-item-left'>
-                  <h1 className='products-item-title'>Авоська</h1>
+              <div className="products-item-info">
+                <div className="products-item-wrapper">
+                  <div className="products-item-left">
+                    <h1 className="products-item-title">Авоська</h1>
+                  </div>
+                  <div className="products-item-right">
+                    <a className="products-item-arrow" href="#">
+                      <img src={arrow} alt="arrow" />
+                    </a>
+                  </div>
                 </div>
-                <div className='products-item-right'>
-                  <a className='products-item-arrow' href='#'>
-                    <img src={arrow} alt='arrow' />
-                  </a>
-                  <span className='products-item-price'>120 грн</span>
-                </div>
+                <span className="products-item-price">120 грн</span>
               </div>
             </li>
-            <li className='products-item'>
-              <a className='products-item-img' href='#'>
+            <li className="products-item">
+              <a className="products-item-img" href="#">
                 <img
-                  src={require('../../assets/images/products/prod-1.jpg')}
-                  alt='product'
+                  src={require("../../assets/images/products/prod-1.jpg")}
+                  alt="product"
                 />
               </a>
-              <div className='products-item-info'>
-                <div className='products-item-left'>
-                  <h1 className='products-item-title'>Набір “АКТУАЛЬНИЙ”</h1>
-                  <p className='products-item-prod'>Авоська</p>
-                  <p className='products-item-prod'>Шопер</p>
-                  <p className='products-item-prod'>Захисна маска</p>
+              <div className="products-item-info">
+                <div className="products-item-wrapper">
+                  <div className="products-item-left">
+                    <h1 className="products-item-title">Набір “АКТУАЛЬНИЙ”</h1>
+                    <p className="products-item-prod">Авоська</p>
+                    <p className="products-item-prod">Шопер</p>
+                    <p className="products-item-prod">Захисна маска</p>
+                  </div>
+                  <div className="products-item-right">
+                    <a className="products-item-arrow" href="#">
+                      <img src={arrow} alt="arrow" />
+                    </a>
+                  </div>
                 </div>
-                <div className='products-item-right'>
-                  <a className='products-item-arrow' href='#'>
-                    <img src={arrow} alt='arrow' />
-                  </a>
-                  <span className='products-item-price'>520 грн</span>
-                </div>
+                <span className="products-item-price">520 грн</span>
               </div>
             </li>
           </ul>
--- src/components/MainPage/Main.tsx
+++ src/components/MainPage/Main.tsx
@@ -1,32 +1,34 @@
-import Grechno from '../../assets/images/Grechno.svg';
-import facebook from '../../assets/images/facebook.svg';
-import instagram from '../../assets/images/instagram.svg';
-import main from '../../assets/images/main.jpg';
+import Grechno from "../../assets/images/Grechno.svg";
+import facebook from "../../assets/images/facebook.svg";
+import instagram from "../../assets/images/instagram.svg";
+import main from "../../assets/images/main.png";
 
 const Main = () => {
   return (
-    <div className='main'>
-      <div className='main__container'>
-        <div className='main__wrapper'>
-          <div className='main__content'>
-            <img className='main__content-img' src={Grechno} alt='grechno' />
-            <p className='main__content-text'>
-              Сумки, створені бути поруч у всіх випадках життя
-            </p>
-            <button className='main__content-button button'>
-              ДИВИТИСЯ КАТАЛОГ
-            </button>
-            <div className='main__content-social'>
-              <a href='#'>
-                <img src={facebook} alt='facebook' />
-              </a>
-              <a href='#'>
-                <img src={instagram} alt='facebook' />
-              </a>
+    <div className="main">
+      <div className="main__fluid">
+        <div className="main__container">
+          <div className="main__wrapper">
+            <div className="main__content">
+              <img className="main__content-img" src={Grechno} alt="grechno" />
+              <p className="main__content-text">
+                Сумки, створені бути поруч у всіх випадках життя
+              </p>
+              <button className="main__content-button button">
+                ДИВИТИСЯ КАТАЛОГ
+              </button>
+              <div className="main__content-social">
+                <a href="#">
+                  <img src={facebook} alt="facebook" />
+                </a>
+                <a href="#">
+                  <img src={instagram} alt="facebook" />
+                </a>
+              </div>
             </div>
           </div>
-          <img className='main__decor' src={main} alt='main' />
         </div>
+        <img className="main__decor" src={main} alt="main" />
       </div>
     </div>
   );
